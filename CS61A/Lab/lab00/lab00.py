a8726f1df1d674952d4307d351cf8e93913feeb1
hygge <2078897125@qq.com>
2025-03-21 12:08:12 +0800

    cs61A part

---
+++ CS61A/Lab/lab00/lab00.py
@@ -0,0 +1,10 @@
+def twenty_twenty_four():
+    """Come up with the most creative expression that evaluates to 2024
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_four()
+    2024
+    """
+    target = 2 * 1000 + 2 * 10 + 4
+    return target
+
